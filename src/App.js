94a71494039a002350e9c662513de9f5d1fca484
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2022-10-13 16:22:04 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -62,9 +62,12 @@ function App() {
                 <AiFillGithub />
               </a>
             </div>
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <div className="flex" >
+                <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative items-center overflow-hidden mt-20 md:h-96 md:w-96">
               <img src={Tshembani}  layout="fill" objectFit="contain" />
             </div>
+            </div>
+          
           </div>
         </section>
         <section>
