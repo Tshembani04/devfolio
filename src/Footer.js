d4ab840ba4abd58af7090d0440a449b0e87d553b
Tshembani04 <aurthurmhlongo.ta@gmail.com>
2022-11-21 13:20:10 +0200

    coming

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <a className="text-gray-800" href="/privacy-policy">
+      Privacy Policy
+    </a>
+  );
+}
+
+export default Footer;
